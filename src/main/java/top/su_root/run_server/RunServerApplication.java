0b548fc42a509726155c0346c43cd8a88849b3ff
stablestaple <sdzzy17186@163.com>
2022-05-30 11:25:56 +0800

    initial files

---
+++ src/main/java/top/su_root/run_server/RunServerApplication.java
@@ -0,0 +1,13 @@
+package top.su_root.run_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/su_root/run_server/RunServerApplicationTests.java
@@ -0,0 +1,13 @@
+package top.su_root.run_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
